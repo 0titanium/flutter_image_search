50746c7f95bcfbe1f833a3f82cbb325b3c6a5ad7
kimhyungmin <jeen14rla@gmail.com>
2024-04-11 16:10:39 +0900

    feat: add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_search/data/api/photo_api.dart';
+import 'package:flutter_image_search/data/repository/photo_repository.dart';
+import 'package:flutter_image_search/data/repository/photo_repository_impl.dart';
+import 'package:flutter_image_search/presentation/search_list/search_list_screen.dart';
+import 'package:flutter_image_search/presentation/search_list/search_list_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,15 +13,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-    @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-                                                                                                                                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Container(),
+      home: ChangeNotifierProvider(
+        create: (_) => SearchListViewModel(
+          PhotoRepositoryImpl(PhotoApi()),
+        ),
+        child: SearchListScreen(),
+      ),
     );
   }
 }
