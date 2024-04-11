b0a8921576239c050452863d535ac4fb7528d39d
kimhyungmin <jeen14rla@gmail.com>
2024-04-11 16:11:09 +0900

    feat: add search_list_screen ui

---
+++ lib/presentation/search_list/search_list_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_image_search/presentation/components/image_card_widget.dart';
+import 'package:flutter_image_search/presentation/search_list/search_list_view_model.dart';
+import 'package:provider/provider.dart';
+
+class SearchListScreen extends StatefulWidget {
+  const SearchListScreen({super.key});
+
+  @override
+  State<SearchListScreen> createState() => _SearchListScreenState();
+}
+
+class _SearchListScreenState extends State<SearchListScreen> {
+  final _queryTextEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final viewModel = context.watch<SearchListViewModel>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('image search 1'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                hintText: 'search word...',
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {
+                    final query = _queryTextEditingController.text;
+                    viewModel.onSearch(query);
+                  },
+                )),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: viewModel.isLoading
+                  ? const Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : GridView.count(
+                      crossAxisCount: 10,
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 2,
+                      children: viewModel.photos
+                          .map((e) => ImageCardWidget(photo: e))
+                          .toList(),
+                    ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
