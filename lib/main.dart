c61dd58225df9964403d2c264e972b5f62d30721
kimhyungmin <jeen14rla@gmail.com>
2024-04-11 16:30:40 +0900

    chore: remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_image_search/data/api/photo_api.dart';
-import 'package:flutter_image_search/data/repository/photo_repository.dart';
 import 'package:flutter_image_search/data/repository/photo_repository_impl.dart';
 import 'package:flutter_image_search/presentation/search_list/search_list_screen.dart';
 import 'package:flutter_image_search/presentation/search_list/search_list_view_model.dart';
