ec81f7b16040fa5f1c2cab8141a45587cad09992
kimhyungmin <jeen14rla@gmail.com>
2024-04-12 15:14:11 +0900

    feat: change viewModel in main

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_image_search/data/repository/photo_repository_impl.dart'
 // import 'package:flutter_image_search/presentation/search_list/search_list_screen.dart';
 import 'package:flutter_image_search/presentation/search_list/search_list_view_model.dart';
 import 'package:flutter_image_search/presentation/search_list_state_holder_version/search_list_screen_v1.dart';
+import 'package:flutter_image_search/presentation/search_list_state_holder_version/search_list_view_model_v1.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: ChangeNotifierProvider(
-        create: (_) => SearchListViewModel(
+        create: (_) => SearchListViewModelV1(
           PhotoRepositoryImpl(PhotoApi()),
         ),
         child: const SearchListScreenV1(),
