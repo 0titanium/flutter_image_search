2c82ab1b0b042101c54234b7130d228c9c6773d4
kimhyungmin <jeen14rla@gmail.com>
2024-04-12 14:44:27 +0900

    feat: display search_list_screen_v1 in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_image_search/data/api/photo_api.dart';
 import 'package:flutter_image_search/data/repository/photo_repository_impl.dart';
-import 'package:flutter_image_search/presentation/search_list/search_list_screen.dart';
+// import 'package:flutter_image_search/presentation/search_list/search_list_screen.dart';
 import 'package:flutter_image_search/presentation/search_list/search_list_view_model.dart';
+import 'package:flutter_image_search/presentation/search_list_state_holder_version/search_list_screen_v1.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         create: (_) => SearchListViewModel(
           PhotoRepositoryImpl(PhotoApi()),
         ),
-        child: const SearchListScreen(),
+        child: const SearchListScreenV1(),
       ),
     );
   }
